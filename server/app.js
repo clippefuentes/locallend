922a07aee42ae84c855e3896c3089dd80a0653de
Clyne Fuentes <clippefuentes@gmail.com>
2023-08-27 21:38:06 -0600

    setup scaffolding

---
+++ server/app.js
@@ -0,0 +1,27 @@
+import path from 'path';
+import Koa from 'koa';
+import serve from 'koa-static';
+import Router from 'koa-router';
+import mongoose from 'mongoose';
+
+const app = new Koa();
+const router = new Router();
+
+// Serve static files
+app.use(serve(path.resolve('../dist')));
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/locallend', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Define routes
+router.get('/api', (ctx, next) => {
+  ctx.body = 'Hello World from API!';
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
