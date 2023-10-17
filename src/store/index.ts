a136a0d4911a46bddc49ba2ae8afe3b9a5e58400
Clyne Fuentes <clippefuentes@gmail.com>
2023-10-16 21:07:05 -0600

    change store object

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,17 @@
 import { createStore } from 'vuex'
 
 const store = createStore({
-  state () {
-    return {
-      count: 0
-    }
+  state: {
+    count: 0
   },
   mutations: {
     increment (state) {
       state.count++
+    }
+  },
+  actions: {
+    increment (context) {
+      context.commit('increment')
     }
   }
 })
